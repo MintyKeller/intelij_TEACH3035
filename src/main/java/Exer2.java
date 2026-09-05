128f60d2d86197ca6de9ab01bd6b020614e721ba
MintyKeller <talitakeller8@gmail.com>
2026-09-05 15:31:44 -0300

    "Exercicio 2"

--- src/main/java/Exer2.java
+++ src/main/java/Exer2.java
@@ -6,7 +6,7 @@ public class Exer2 {
     /*Exercício 2- Peça para que usuário informe números, através do
     Scanner, e faça algum tipo de cálculo. Podendo qualquer uma das
     operações aprendidas em aula, em seguida printar o resultado no
-    console.*/
+    console.      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Insira dois números inteiros para soma: ");
