ad711ef1a69fd1b045ffa44240657ef4684a2f7e
MintyKeller <talitakeller8@gmail.com>
2026-09-05 15:22:47 -0300

    "exercio 1"

---
+++ src/main/java/Exer1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Exer1 {
+
+    public static void main(String[] args) {
+        /*Exercício 1- Peça para que o usuário informe seu nome e idade,
+    logo após print no console as informações de uma forma
+    explicativa.*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira o seu nome:");
+        String nome = sc.next();
+        System.out.println("Insira a sua idade:");
+        int idade = sc.nextInt();
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+
+
+
+
+    }
+}
